24e44ac5814f79415c3fba1f7eb03656a6830977
hojiii <shimn3060@naver.com>
2023-06-23 18:59:38 +0900

    feat: fetch를 이용할때 sgg방법,주기적인ISR을하는법,서버요청시시ssr하는법

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,10 +1,17 @@
 import { getProducts } from '@/service/products';
 import Link from 'next/link';
+import styles from './page.module.css'
 
-export const revalidate = 3;
+// export const revalidate = 3;
 
 export default async function ProductsPage() {
   const products = await getProducts();
+  const res = await fetch('https://meowfacts.herokuapp.com',{
+    next : {revalidate: 3},
+    //cache: '' 이렇게하면 force-cache가 디폴트 임으로 ssg 로 행동, no-store로 해도 ssg로 행동 캐시를 해두지 않아서 서버에 요청이 오면 드때 패치
+  });
+  const data = await res.json();
+  const factText = data.data[0]
   return (
     <>
       <h1>제품 소개 페이지!</h1>
@@ -15,6 +22,9 @@ export default async function ProductsPage() {
           </li>
         ))}
       </ul>
+      <article className={styles.article}>
+        {factText}
+      </article>
     </>
   );
 }

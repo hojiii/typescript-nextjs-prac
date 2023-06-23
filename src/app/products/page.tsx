fbb4646186c39dc0c69f92d1ff0183718e203bff
hojiii <shimn3060@naver.com>
2023-06-23 20:50:49 +0900

    feat: 고양이얘기 CSR로 변환

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,3 +1,5 @@
+import MeowArticle from '@/components/MeowArticle';
+import MeowdArticle from '@/components/MeowArticle';
 import { getProducts } from '@/service/products';
 import Link from 'next/link';
 import styles from './page.module.css'
@@ -6,12 +8,6 @@ import styles from './page.module.css'
 
 export default async function ProductsPage() {
   const products = await getProducts();
-  const res = await fetch('https://meowfacts.herokuapp.com',{
-    next : {revalidate: 3},
-    //cache: '' 이렇게하면 force-cache가 디폴트 임으로 ssg 로 행동, no-store로 해도 ssg로 행동 캐시를 해두지 않아서 서버에 요청이 오면 드때 패치
-  });
-  const data = await res.json();
-  const factText = data.data[0]
   return (
     <>
       <h1>제품 소개 페이지!</h1>
@@ -22,9 +18,7 @@ export default async function ProductsPage() {
           </li>
         ))}
       </ul>
-      <article className={styles.article}>
-        {factText}
-      </article>
+      <MeowArticle/>
     </>
   );
 }
